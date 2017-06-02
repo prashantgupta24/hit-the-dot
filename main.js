27b6cf66eb80319ad4fe3b29d459b95f2379e63c
Prashant Gupta <prashantgupta@us.ibm.com>
2017-06-01 23:28:04 -0700

    added logic

--- main.js
+++ main.js
@@ -1,51 +1,96 @@
+var playing = false;
+var defaultGameLength = 5;
+var setIntFunction;
+var radios;
+var random;
+
 $(function(){
   console.log('ready');
-  $("form input:radio").on("click", function(){
+  radios = $("form input:radio").on("click", function(){
     if(!playing) {
       $(this).prop("checked", false);
     } else{
-      console.log($(this).val());
+      var head = $(this).val();
+      //console.log(head);
+      var score = $("#score").val() != 0 ? $("#score").val():0;
+      if(head==random){
+        $("#score").val(++score);
+        $(this).prop("checked", false);
+        placehead();
+      }
+      else {
+        $("#score").val(--score);
+        $(this).prop("checked", false);
+      }
     }
-  })
+  });
+  console.log(radios.length);
 });
-
-var playing = false;
-var defaultGameLength = 20;
-var setIntFunction;
 
 var play = function(){
   playing = playing? false:true;
   console.log("playing " + playing);
-  var time = defaultGameLength;
+  if(playing){
+    var time = defaultGameLength;
+    $("#score").val(0);
+    decTime(time--);
+    setIntFunction = setInterval(function(){
+      decTime(time--)
+    }, 1000);
 
-  // setIntFunction = setInterval(function(){
-  //   decTime(time--)
-  // }, 1000);
+    placehead();
+  }
+  else {
+    resetPlay();
+  }
+
   //var radios = $("form").find("tr").find("input").filter(":radio");
   //$( "input[type=checkbox]" ).on( "click", countChecked );
-  var radios = $("form input:radio");
+  //var radios = $("form input:radio");
   //radios[4].prop("checked", true);
 
-  //console.log(radios);
+//  console.log(radios);
 
+   //var radio = radios[4];
+  // console.log(radio);
+  // radio.attr("checked", true);
+// var i=0;
+//   radios.each(function(){
+//     if(i==4){
+//       $(this).prop('checked', true);
+//     }
+//     i++;
+//   })
 
 
 }
 
 var decTime = function(time){
-  console.log('decreasing time starting with ' + time);
+  //console.log('decreasing time starting with ' + time);
   if(time == 0) {
-      clearInterval(setIntFunction);
+      resetPlay();
   }
   $("#timeleft").val(time);
 }
 
-// var hithead = function(head){
-//   //console.log(head);
-// }
-
 var placehead = function(){
-  var random = Math.floor(Math.random() * 10)+1;
+  random = Math.floor(Math.random() * radios.length);
+  //console.log(random);
+  var i=0;
+    radios.each(function(){
+      if(i==random){
+        $(this).prop('checked', true);
+      }
+      i++;
+    })
+}
 
-  document.dmz.elements[id].checked=false;
+var resetPlay = function(){
+  alert("Your score was " + $("#score").val());
+  clearInterval(setIntFunction);
+  playing = false;
+  radios.each(function(){
+      $(this).prop('checked', false);
+  });
+   $("#timeleft").val('');
 }
