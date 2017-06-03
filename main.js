28a2e4bfaec24553446956d41beed100036fd3a2
Prashant Gupta <prashantgupta@us.ibm.com>
2017-06-02 17:53:37 -0700

    reducing global variables

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-let playing = false;
-let defaultGameLength = 5;
-let setIntFunction;
-let radios;
-let random;
-
 $(function() {
   //console.log('ready');
-  radios = $('form input:radio').on('click', function() {
+  let playing = false;
+  let defaultGameLength = 5;
+  let setIntFunction;
+  let random;
+
+  $('#startstop').on('click', function () {
+    play();
+  });
+
+  let radios = $('form input:radio').on('click', function() {
     if (!playing) {
       $(this).prop('checked', false);
     } else {
@@ -23,45 +26,45 @@ $(function() {
       }
     }
   });
-});
 
-let play = function() {
-  playing = playing ? false : true;
-  //console.log('playing ' + playing);
-  if (playing) {
-    let time = defaultGameLength;
-    $('#score').val(0);
-    decTime(time--);
-    setIntFunction = setInterval(function() {
+  let play = function() {
+    playing = playing ? false : true;
+    //console.log('playing ' + playing);
+    if (playing) {
+      let time = defaultGameLength;
+      $('#score').val(0);
       decTime(time--);
-    }, 1000);
+      setIntFunction = setInterval(function() {
+        decTime(time--);
+      }, 1000);
 
-    placehead();
-  } else {
-    resetPlay();
-  }
-};
+      placehead();
+    } else {
+      resetPlay();
+    }
+  };
 
-let decTime = function(time) {
-  //console.log('decreasing time starting with ' + time);
-  if (time == 0) {
-    resetPlay();
-  }
-  $('#timeleft').val(time);
-};
+  let decTime = function(time) {
+    //console.log('decreasing time starting with ' + time);
+    if (time == 0) {
+      resetPlay();
+    }
+    $('#timeleft').val(time);
+  };
 
-let placehead = function() {
-  random = Math.floor(Math.random() * radios.length);
-  //console.log(random);
-  radios.eq(random).prop('checked', true);
-};
+  let placehead = function() {
+    random = Math.floor(Math.random() * radios.length);
+    //console.log(radios);
+    radios.eq(random).prop('checked', true);
+  };
 
-let resetPlay = function() {
-  alert('Your score was ' + $('#score').val());
-  clearInterval(setIntFunction);
-  playing = false;
-  radios.each(function() {
-    $(this).prop('checked', false);
-  });
-  $('#timeleft').val('');
-};
+  let resetPlay = function() {
+    alert('Your score was ' + $('#score').val());
+    clearInterval(setIntFunction);
+    playing = false;
+    radios.each(function() {
+      $(this).prop('checked', false);
+    });
+    $('#timeleft').val('');
+  };
+});
